7ab225e7aea758bb00a0a8b4942816c2f0036b4f
eruditej <eruditej@901stjames.com>
2017-12-26 20:22:21 -0600

    update configure.sh for cuda9.x builds, but leave breadcrumbs for people on old platforms

--- configure.sh
+++ configure.sh
@@ -1,10 +1,17 @@
-# possible additional CUDA_CFLAGS
-#-gencode=arch=compute_50,code=\"sm_50,compute_50\"
-#-gencode=arch=compute_35,code=\"sm_35,compute_35\"
-#-gencode=arch=compute_30,code=\"sm_30,compute_30\"
+#!/bin/bash
+## configure.sh
+## this runs ./configure for you because you wont read it yourself
 
 #--ptxas-options=\"-v -dlcm=cg\""
 
-extracflags="-march=native -std=c++11 -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16 -fno-strict-aliasing"
+# If you are trying to make this work on ubuntu < 17, you may need to swith these lines
+#extracflags="-march=native -std=c++11 -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16 -fno-strict-aliasing" 
+extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16 -fno-strict-aliasing" 
 
-CUDA_CFLAGS="-O3 -std=c++11 -Xcompiler -fno-strict-aliasing,-Wall -D_FORCE_INLINES" ./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
+## This assumes you have cuda installed and symlinked 
+## This is cuda9 branch and you should be on a newer gcc really. So we use the fPIC option.
+## but here is what you should probably use on ubuntu < 17
+#CUDA_CFLAGS='-D_FORCE_INLINES -lineno"
+CUDA_CFLAGS='-lineno --shared --compiler-options "-Wall -fPIC -fno-strict-aliasing"' \
+./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
+
